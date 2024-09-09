fe7ac00b3e6ce2ee1226738db1361a0e6b5706b3
Guilherme de Oliveira Machado <112173925+guilhermeomachadotech@users.noreply.github.com>
2024-09-09 08:06:09 -0300

    Adição de atualização, deletagem e select de usuários através da APÌ do Laravel

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -9,7 +9,72 @@ class CadastroController extends Controller
     public function index(){
 
     }
+    //Função de exibição de todos os dados através do id
 
+    public function show(string $id){
+        //Responsável por buscar todos os registros e listar//
+        $student = Student::get()->toJson(JSON_PRETTY_PRINT);
+        return response($student, 200);
+    }
+
+    //Responsável por buscar cada estudante pelo id//
+
+    public function exibirEstudante($id){
+        if (Student::where('id', $id)->exists()) {
+            $student = Student::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($student, 200);
+        }else{
+            return response()->json([
+                'message'=> 'Estudante não encontrado'
+            ], 404);
+        }
+    }
+
+
+    //Função para atualizar o estudante por id
+
+    public function update(Request $request, string $id){
+        if (Student::where('id', $id)->exists()) {
+            $student = Student::find($id);
+            //Verificar se os campos não estão nulos
+
+            $student->nome = is_null($request->nome) ? $student->nome : $request->nome;
+            $student->curso = is_null($request->curso) ? $student->curso : $request->curso; 
+
+            //Salvar no banco
+
+            $student->save();
+
+            return response()->json([
+                'message' => 'Atualização do estudante do id '.$id.' realizada com sucesso!'
+            ], 200);
+        }else{
+            return response()->json([
+                'message' => 'Estudante não encontrado!'
+            ], 404);
+        }
+    }
+
+
+    //Função para deletar o usuário pelo id
+
+    public function destroy(string $id){
+        //Verificar se existe usuário
+
+        if (Student::where('id', $id)->exists()) {
+            $student = Student::find($id);
+
+            $student ->delete();
+
+            return response()->json([
+                'message' => 'Estudante do id '.$id.' deletado com sucesso!'
+            ], 202);
+        }else{
+            return response()->json([
+                'message' => 'Estudante não encontrado!'
+            ], 404);
+        }
+    }
     //Função para cadastrar o Aluno
     
     public function store(Request $request){
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CadastroController;
 
 
 //Construir a rota do tipo post e aponta para o cadastro Controlle e seu métodod de cadastrar
 Route::post('/estudante',[CadastroController::class , 'store']);
+
+//Rota para listar o usuário atráves do seu id
+
+Route::get('/estudante/{id}', [CadastroController::class, 'show']);
+
+//Exibir todos os dados por estudante
+
+Route::get('/estudante/{id}', [CadastroController::class, 'exibirEstudante']);
+
+//Atualizar o usuário
+
+Route::put('/estudante/{id}', [CadastroController::class, 'update']);
+
+//Deletar o estudante
+
+Route::delete('/estudante/{id}', [CadastroController::class, 'destroy']);
